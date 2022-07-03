ed88214bc0ba877300c05ad149260fad05c3f917
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-03 01:25:34 -0700

    Write a C program that prints exact string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the string with proper grammar, but the outcome is a piece of art, 
+ * main - prints the string to std output using printf
  *
  * Return: returns 0 if every thing is OK
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
