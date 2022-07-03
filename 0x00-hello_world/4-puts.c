fdc5a2d6270aab3edc07f030062141093b1e23a8
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-02 17:56:10 -0700

    Write a C program that prints an exact word

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+
+/**
  * Use the function puts to print text to standard output
  */
-
-int main(){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
