262890aaa94255c394687554eec94f61a1deba29
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 05:10:16 -0700

    Check last digit of a decimal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - the purpose of this program is to
  * assign a random number to the variable n
- * each time it is executed and checks the 
+ * each time it is executed and checks the
  * last digit
  *
  * Return: returns 0 if everything is o.k
@@ -15,7 +15,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d", n, n % 10);
+printf("Last digit of %d is %d ", n, n % 10);
 if (n % 10 > 5)
 printf("and is greater than 5\n");
 else if (n % 10 == 0)
