c2cea1a0bb955a96bb92a24c9e42ad825378184d
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 14:35:04 -0700

    Print lowercase letters in reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 /**
  * main - the purpose of this program is to
- * print lowercase letters in reverse order 
+ * print lowercase letters in reverse order
  *
  * Return: returns 0 if everything is o.k
  */
