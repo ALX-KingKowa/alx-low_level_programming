7f44f930941d1b3361eda6db54e1ad4ac00c0e9c
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 03:51:15 -0700

    Write a program that assigns a random number to a variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the purpose of this program is to
+ * assign a random number to the variable n
+ * each time it is executed.
+ *
+ * Return: returns 0 if everything is o.k
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n>0)
+ printf("%d is positive\n", n);
+else if (n==0)
+ printf("%d is zero\n" , n);
+else if (n<0)
+ printf("%d is negative\n", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+         PROJECT TASK                          DESCRIPTION
+      0x01-variables_if_else_while            Assign a random number to a variable each time it is executed
