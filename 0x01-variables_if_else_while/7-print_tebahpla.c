bd7b64656fc2fb34864241c662a8418bff4b0bab
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 14:31:07 -0700

    Print lowercase letters in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the purpose of this program is to
+ * print lowercase letters in reverse order 
+ *
+ * Return: returns 0 if everything is o.k
+ */
+int main(void)
+{
+int i;
+for (i = 122; i > 96 ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
