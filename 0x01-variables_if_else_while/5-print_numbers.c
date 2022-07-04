6a079ef032c2eb73c8734a625fcccf2ea3ef3c6c
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 08:34:03 -0700

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the purpose of this program is to
+ * print numbers from zero to nine(9)
+ *
+ * Return: returns 0 if everything is o.k
+ */
+int main(void)
+{
+for (int i = 0; i < 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
