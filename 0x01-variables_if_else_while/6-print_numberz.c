3bb69c78926b5dbc222f16cdea0b3ef60a31c4d2
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-04 13:46:08 -0700

    Print single digit number with a base of 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the purpose of this program is to
+ * print numbers from zero to nine(9)
+ *
+ * Return: returns 0 if everything is o.k
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
