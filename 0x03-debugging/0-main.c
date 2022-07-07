b2e7b40c5fcea7bfc46ddb3365727dc290241aa5
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-07 09:22:07 -0700

    Test positive or negative function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,7 @@
  * main - tests function that prints if
  * integer is positive or negative
  *
- * positive_or_negative - function to test 
+ * positive_or_negative - function to test
  * positive or negative integer
  *
  * Return: 0
