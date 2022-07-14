2d7ed7b77b8c5778d7eb0a728aeeb582a0ccfcd2
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-14 15:08:10 -0700

    Write a function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,13 +4,14 @@
  * _strcmp - this function compare two strings
  * @s1: pointer to the first string
  * @s2: pointer to the second string
+ * @n: number of bytes
  * Return:
  * returns zero if s1 == s2
  * returns negative number if s1 < s2
  * returns positive number if s1 > s2
  */
 
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0, diff = 0;
 
@@ -18,8 +19,11 @@ while (diff == 0)
 {
 if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 break;
+while (i < n)
+{
 diff = *(s1 + i) - *(s2 + i);
 i++;
+}
 }
 
 return (diff);
