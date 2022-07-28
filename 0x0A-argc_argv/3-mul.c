5b8fbe9714fc7f5173855b163bf460594312c2c9
Frederick <ankomahfrederickyahoo@gmail.com>
2022-07-27 22:32:55 -0700

    Write a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,12 @@
 
 int main(int argc, char *argv[])
 {
-int a, b, result;
 
 if (argc <= 2)
 {
 printf("Error\n");
 return (1);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-result = a * b;
-printf("%d\n", result);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
