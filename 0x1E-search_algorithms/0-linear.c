c091a7c2ffb884fa071df38bc7320bd84562c753
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2023-01-27 11:07:28 +0000

    What is the time complexity of a linear search of an array of n size

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that searches for a particular value in an array
+ *
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+}
