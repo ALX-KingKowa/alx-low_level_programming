585a4d05d312c5555ce05d907a844fd432ef3631
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2023-01-29 10:17:46 +0000

    Write a function to search for a value in an array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,9 +2,22 @@
 /**
  * linear_search - function that searches for a particular value in an array
  *
+ * @array: input array
+ * @size: size of the array
+ * @value: the value to search for
+ * Return: success
  *
  */
 int linear_search(int *array, size_t size, int value)
 {
-
+int i;
+if (array == NULL)
+	return (-1);
+for (i = 0; i < (int)size; i++)
+{
+	printf("Value checked array[%d] = [%d]\n");
+	if (value == array[i])
+		return (i)
+}
+return (-1)
 }
